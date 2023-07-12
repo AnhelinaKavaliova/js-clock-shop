ea0a3d2fa62b8208a7bb86866e5f166f215ecd05
AnhelinaKavaliova <angelinakovaleva228@gmail.com>
2023-07-12 19:44:08 +0200

    Created catalog

---
+++ shopApp/consts/catalog.js
@@ -0,0 +1,65 @@
+const CATALOG = [
+    {
+        id: 'prod1',
+        name: 'HOWARD MILLER CASSIDY BLACK COFFEE MANTEL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635198.jpg',
+        price: 289000,
+    },
+
+    {
+        id: 'prod2',
+        name: 'HERMLE ISABELLA MECHANICAL REGULATOR WALL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Hermle%2071001030341.jpg',
+        price: 898000,
+    },
+
+    {
+        id: 'prod3',
+        name: 'HOWARD MILLER ENZO WALL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20620508.jpg',
+        price: 1549000,
+    },
+
+    {
+        id: 'prod4',
+        name: 'PENDULUX ROVER TABLE CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Pendulux%20Rover%20Table.jpg',
+        price: 229000,
+    },
+
+    {
+        id: 'prod5',
+        name: 'ENAMEL 26.75 INCH COFFEE CUP KITCHEN WALL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Enamel%2026.7%20inch%20coffee%20cup.jpg',
+        price: 99000,
+    },
+
+    {
+        id: 'prod6',
+        name: 'GALVANIZED 18.25″ WALL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Galvanized%2018.25.jpg',
+        price: 58000,
+    },
+
+    {
+        id: 'prod7',
+        name: 'HOWARD MILLER ASPEN 22 INCH OUTDOOR WALL CLOCK ' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20625677.jpg',
+        price: 199000,
+    },
+
+    {
+        id: 'prod8',
+        name: 'BULOVA THE KINGSTON CONTEMPORARY MANTEL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Bulova%20the%20kingston.jpg',
+        price: 124000,
+    },
+
+    {
+        id: 'prod9',
+        name: 'HOWARD MILLER GLOBAL TIME MANTEL CLOCK' ,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635212.jpg',
+        price: 219000,
+    }
+
+];
