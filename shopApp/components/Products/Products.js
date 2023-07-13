af06ffcd59f6636f3f594199332052f30c407c45
AnhelinaKavaliova <angelinakovaleva228@gmail.com>
2023-07-13 09:53:47 +0200

    Implemented catalog rendering mechanism

---
+++ shopApp/components/Products/Products.js
@@ -0,0 +1,31 @@
+class Products{
+    
+    render(){
+
+        let htmlCatalog = '';
+        CATALOG.forEach(({id, name, img, price})=>{
+            
+            htmlCatalog += `
+                <li>
+                    <span>${name}</span>
+                    <img src="${img}"/>
+                    <span>${price}</span>
+                    <button>Add to Basket</button>
+                </li>
+            `;
+        });
+
+        const html = `
+            <u>
+                ${htmlCatalog}
+            </ul>
+        
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
+
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
--- shopApp/consts/catalog.js
+++ shopApp/consts/catalog.js
@@ -2,63 +2,62 @@ const CATALOG = [
     {
         id: 'prod1',
         name: 'HOWARD MILLER CASSIDY BLACK COFFEE MANTEL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635198.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635198.jpg?raw=true',
         price: 289000,
     },
 
     {
         id: 'prod2',
         name: 'HERMLE ISABELLA MECHANICAL REGULATOR WALL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Hermle%2071001030341.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Hermle%2071001030341.jpg?raw=true',
         price: 898000,
     },
 
     {
         id: 'prod3',
         name: 'HOWARD MILLER ENZO WALL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20620508.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20620508.jpg?raw=true',
         price: 1549000,
     },
 
     {
         id: 'prod4',
         name: 'PENDULUX ROVER TABLE CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Pendulux%20Rover%20Table.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Pendulux%20Rover%20Table.jpg?raw=true',
         price: 229000,
     },
 
     {
         id: 'prod5',
         name: 'ENAMEL 26.75 INCH COFFEE CUP KITCHEN WALL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Enamel%2026.7%20inch%20coffee%20cup.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Enamel%2026.7%20inch%20coffee%20cup.jpg?raw=true',
         price: 99000,
     },
 
     {
         id: 'prod6',
         name: 'GALVANIZED 18.25″ WALL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Galvanized%2018.25.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Galvanized%2018.25.jpg?raw=true',
         price: 58000,
     },
 
     {
         id: 'prod7',
         name: 'HOWARD MILLER ASPEN 22 INCH OUTDOOR WALL CLOCK ' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20625677.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20625677.jpg?raw=true',
         price: 199000,
     },
 
     {
         id: 'prod8',
         name: 'BULOVA THE KINGSTON CONTEMPORARY MANTEL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Bulova%20the%20kingston.jpg',
-        price: 124000,
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Bulova%20the%20kingston.jpg?raw=true'
     },
 
     {
         id: 'prod9',
         name: 'HOWARD MILLER GLOBAL TIME MANTEL CLOCK' ,
-        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635212.jpg',
+        img: 'https://github.com/AnhelinaKavaliova/js-clock-shop/blob/main/shopApp/img/catalog/Howard%20635212.jpg?raw=true',
         price: 219000,
     }
 
